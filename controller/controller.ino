ff585bcfa3fa9b9e71ccf51882ed8923741c96f2
kelvinyuan2004 <kelvinyuan2004@gmail.com>
2025-05-06 12:16:32 -0700

    update visual to scoring system

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,12 +1,17 @@
 #include "pinDefinitions.h"
 #include "display.h"
 
-unsigned long lastEdgeTime;
 int score;
-bool hasCounted;
-unsigned long startupDelay;
+bool scoreUpdated;
+
 unsigned long startTime;
-unsigned long lastDisplaySwitch;
+unsigned long lastEdgeTime;
+unsigned long lastPoll;
+unsigned long lastDisplayUpdate;
+
+const int startupDelay = 1000; // startup delay for polling system
+const int pollRate = 50; // polling rate for visual sensor (ms)
+const int edgeBuffer = 20; // buffer for the edge updates
 
 void setup()
 {
@@ -24,53 +29,57 @@ void setup()
 	pinMode(NPN_PIN1, OUTPUT);    // NPN1 Control
 	pinMode(NPN_PIN2, OUTPUT);    // NPN2 Control
 
-	// setup variables/misc
-	Serial.begin(9600);
+	// setup initial states, variables/, misc.
+	score = 0;
+	scoreUpdated = 0;
 
 	startTime = millis();
-	lastEdgeTime = 0;
-	score = 0;
-	hasCounted = 0;
-	startupDelay = 1000;
-	lastDisplaySwitch = 0;
+	lastEdgeTime = millis();
+	lastPoll = millis();
+	Serial.begin(9600);
 }
 
 void loop()
 {
-	static unsigned long lastCheck = 0;
+	visualSensor();
+}
+
+void visualSensor()
+{
 	unsigned long currentTime = millis();
 
-	if (currentTime - lastDisplaySwitch > 5)
+	if (currentTime - startTime < startupDelay) return;
+	
+	if (currentTime - lastDisplayUpdate >= 5)
 	{
 		displayDigits(DS_PIN, SHCP_PIN, STCP_PIN, NPN_PIN1, NPN_PIN2, score/10, score%10);
-    lastDisplaySwitch = currentTime;
-  }
+		lastDisplayUpdate = currentTime;
+	}
 
-	if (currentTime - startTime < startupDelay) return;
+	if (currentTime - lastPoll >= pollRate)
+	{
+    	noInterrupts();
+    	unsigned long timeSinceLastEdge = currentTime - lastEdgeTime;
+    	interrupts();
 
-  if (currentTime - lastCheck > 50) {
-    noInterrupts();
-    unsigned long timeSinceLastEdge = currentTime - lastEdgeTime;
-    interrupts();
+		if(timeSinceLastEdge > edgeBuffer)
+			if (scoreUpdated)
+				scoreUpdated = 0;
+			else
+				updateScore();
 
-    if (timeSinceLastEdge > 20) {
-      if (!hasCounted) {
-        score += 1;
-        if (score > 99) score = 0;
-        Serial.print("Ball Go Through! Score: ");
-        Serial.println(score);
-        hasCounted = true;
-      }
-    } else {
-      hasCounted = false;
-    }
-
-    lastCheck = currentTime;
-  }
+		lastPoll = currentTime;
+	}
 }
 
 void edgeDetected()
 {
-	Serial.println("Fall");
-	lastEdgeTime = millis();
+  lastEdgeTime = millis();
+}
+
+void updateScore()
+{
+	score += 1;
+	if (score > 99) score = 0;				
+	Serial.println("Score updated: " + score);
 }
