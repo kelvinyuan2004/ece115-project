5ad6beecbcf72ad663df54b26f3a1792a5a7fad0
kelvinyuan2004 <kelvinyuan2004@gmail.com>
2025-05-06 11:20:18 -0700

    temp change

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,31 +1,76 @@
 #include "pinDefinitions.h"
 #include "display.h"
 
+unsigned long lastEdgeTime;
+int score;
+bool hasCounted;
+unsigned long startupDelay;
+unsigned long startTime;
+unsigned long lastDisplaySwitch;
+
 void setup()
 {
-	pinMode(DS_PIN, OUTPUT);     // SER
-	pinMode(SHCP_PIN, OUTPUT);   // SRCLK
-	pinMode(STCP_PIN, OUTPUT);   // RCLK
-	pinMode(NPN_PIN1, OUTPUT);  // NPN1 Control
-	pinMode(NPN_PIN2, OUTPUT);  // NPN2 Control
+	// sensor
+	pinMode(VIS_IN_PIN, INPUT);   // visual sensor in
+	attachInterrupt(digitalPinToInterrupt(VIS_IN_PIN), edgeDetected, FALLING);
+	
+	pinMode(VIS_OUT_PIN, OUTPUT); // visual sensor out
+	tone(VIS_OUT_PIN, 1000);
+
+	// display
+	pinMode(DS_PIN, OUTPUT);      // SER
+	pinMode(SHCP_PIN, OUTPUT);    // SRCLK
+	pinMode(STCP_PIN, OUTPUT);    // RCLK
+	pinMode(NPN_PIN1, OUTPUT);    // NPN1 Control
+	pinMode(NPN_PIN2, OUTPUT);    // NPN2 Control
+
+	// setup variables/misc
+	Serial.begin(9600);
+
+	startTime = millis();
+	lastEdgeTime = 0;
+	score = 0;
+	hasCounted = 0;
+	startupDelay = 1000;
+	lastDisplaySwitch = 0;
 }
 
 void loop()
 {
-	delay(500);
+	static unsigned long lastCheck = 0;
+	unsigned long currentTime = millis();
 
-	for (int i = 0; i < 100; i++)
+	if (currentTime - lastDisplaySwitch > 5)
 	{
-		unsigned long start = millis();
+		displayDigits(DS_PIN, SHCP_PIN, STCP_PIN, NPN_PIN1, NPN_PIN2, score/10, score%10);
+    lastDisplaySwitch = currentTime;
+  }
 
-		while (millis() - start < 500UL)
-		{
-			displayDigits(DS_PIN, SHCP_PIN, STCP_PIN, NPN_PIN1, NPN_PIN2,
-										i/10, i%10);
-		}
-		delay(5);
-	}
-	
-	digitalWrite(NPN_PIN1, LOW);
-	digitalWrite(NPN_PIN2, LOW);
+	if (currentTime - startTime < startupDelay) return;
+
+  if (currentTime - lastCheck > 50) {
+    noInterrupts();
+    unsigned long timeSinceLastEdge = currentTime - lastEdgeTime;
+    interrupts();
+
+    if (timeSinceLastEdge > 20) {
+      if (!hasCounted) {
+        score += 1;
+        if (score > 99) score = 0;
+        Serial.print("Ball Go Through! Score: ");
+        Serial.println(score);
+        hasCounted = true;
+      }
+    } else {
+      hasCounted = false;
+    }
+
+    lastCheck = currentTime;
+  }
+}
+
+void edgeDetected()
+{
+	Serial.println("Fall");
+	lastEdgeTime = millis();
 }
--- controller/pinDefinitions.h
+++ controller/pinDefinitions.h
@@ -1,9 +1,14 @@
 #ifndef PIN_DEFS
 #define PIN_DEFS
 
-#define DS_PIN 4
-#define SHCP_PIN 2
-#define STCP_PIN 3
+// visual sensor
+#define VIS_IN_PIN 2
+#define VIS_OUT_PIN 9
+
+// display
+#define DS_PIN 3
+#define SHCP_PIN 5
+#define STCP_PIN 4
 #define NPN_PIN1 6
 #define NPN_PIN2 7
 
